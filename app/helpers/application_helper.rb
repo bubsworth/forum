a3ca9416cb1a9e3c726b2bb13ccce6c211017c87
Beth Hubbard <bah1996@hotmail.co.uk>
2021-09-12 13:50:04 +0100

    Update application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def notification_class(type)
+    case type
+    when "notice"
+      "is-info"
+    when "alert"
+      "is-danger"
+    end
+  end
+
+  def yes_or_no(bool)
+    bool ? "Yes" : "No"
+  end
 end
