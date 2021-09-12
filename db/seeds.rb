00611b15654a9852e1b2cbb5a97484358ad22a3d
Beth Hubbard <bah1996@hotmail.co.uk>
2021-09-12 14:40:52 +0100

    Add post to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.delete_all
+
+post_1 = Post.create!(
+  title: "Gluten-free",
+  content: "Taiyaki truffaut mumblecore, polaroid VHS palo santo craft beer brunch gluten-free kitsch. 
+            Man bun freegan kombucha gluten-free irony dreamcatcher chambray. Actually hot chicken tote bag, 
+            cloud bread kale chips keytar austin pickled cronut banjo kinfolk sriracha cray. Thundercats tousled jean shorts, 
+            gentrify cloud bread fashion axe hot chicken live-edge post-ironic food truck mustache williamsburg 
+            locavore mumblecore cardigan.",
+)
+
+post_2 = Post.create!(
+  title: "Hexagon",
+  content: "Ramps cornhole retro, marfa cardigan occupy 8-bit YOLO scenester. 
+            Cray bespoke celiac, trust fund craft beer church-key four loko typewriter authentic yr cloud bread. 
+            Cloud bread hexagon beard vape. Asymmetrical everyday carry farm-to-table edison bulb man bun kinfolk.",
+)
