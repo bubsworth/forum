8480c5890c1f145b7d386ab9fee0651ddd19c625
Beth Hubbard <beth.hubbard@commit.digital>
2021-09-17 13:27:42 +0100

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
   def index
-    @posts = Post.order("created_at DESC")
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
@@ -11,15 +13,41 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
+      flash[:notice] = "Added new post."
       redirect_to posts_path
     else
-      render 'new'
+      render :new
     end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Post deleted."
+    redirect_to posts_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
